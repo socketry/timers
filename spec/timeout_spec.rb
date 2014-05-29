6e5b94afb478fb594555913edec7e5ad27913a28
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-05-29 15:53:54 +1200

    Add Timers::Timeout and associated spec.

---
+++ spec/timeout_spec.rb
@@ -0,0 +1,32 @@
+
+require 'spec_helper'
+require 'timers/timeout'
+
+describe Timers::Timeout do
+  # Level of accuracy enforced by tests (50ms)
+  Q = 0.05
+
+  it "repeats until timeout expired" do
+    timeout = Timers::Timeout.new(5)
+    count = 0
+    
+    timeout.while_time_remaining do |remaining|
+      expect(remaining).to be_within(Q).of (timeout.duration - count)
+      
+      count += 1
+      sleep 1
+    end
+    
+    expect(count).to eq(5)
+  end
+  
+  it "yields results as soon as possible" do
+    timeout = Timers::Timeout.new(5)
+    
+    result = timeout.while_time_remaining do |remaining|
+      break :done
+    end
+    
+    expect(result).to eq(:done)
+  end
+end
