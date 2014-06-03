18acfe5f8079bef2cc20c775e5668f71a0401619
Tony Arcieri <bascule@gmail.com>
2014-06-03 08:38:40 -0700

    Restore original #wait_interval API

--- lib/timers/group.rb
+++ lib/timers/group.rb
@@ -43,7 +43,7 @@ module Timers
       # Repeatedly call sleep until there is no longer any wait_interval:
       while i = wait_interval
         # We cannot assume that sleep will wait for the specified time, it might be +/- a bit.
-        sleep i
+        sleep i if interval > 0
       end
       
       fire
@@ -53,8 +53,7 @@ module Timers
     def wait_interval(offset = self.current_offset)
       timer = @timers.first
       return unless timer
-      interval = timer.offset - Float(offset)
-      interval > 0 ? interval : nil
+      timer.offset - Float(offset)
     end
 
     # Fire all timers that are ready
